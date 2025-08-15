2eb1c8d91038e555c74efde0a04b3390aa03bf54
TomasLonnblad <tomaslonnblad@gmail.com>
2025-08-15 08:52:52 +0200

    x

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-20 p-24 bg-neutral-900 text-neutral-100">
+      <div className="relative">
+      <button className="opacity-0 bg-neutral-700 px-3 py-2 rounded-md hover:bg-neutral-800 transition-colors duration-200 ">
+      
+        Subscribe to Built With Code
+      </button>
+      <div className="absolute -inset-0 rounded-md bg-yellow-400"/>
+    </div>
+  </main>
+);}
